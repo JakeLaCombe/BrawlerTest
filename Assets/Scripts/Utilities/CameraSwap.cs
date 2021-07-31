3915c4ff6acb8644c966c0a9d2f47800e1fe50bf
JakeLaCombe <jake.lacombe2@gmail.com>
2021-07-30 19:48:33 -0500

    Fix Bug with Camera Swap

--- Assets/Scripts/Utilities/CameraSwap.cs
+++ Assets/Scripts/Utilities/CameraSwap.cs
@@ -8,7 +8,9 @@ public class CameraSwap
    public static void SwapCamera(CinemachineVirtualCamera nextCamera)
     {
        CinemachineBrain brain = Camera.main.GetComponent<CinemachineBrain>();
+       brain.ActiveVirtualCamera.Priority = 0;
        brain.ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+       nextCamera.Priority = 10;
        nextCamera.gameObject.SetActive(true);       
     }
 }
